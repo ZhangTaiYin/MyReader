d9ab79ef922ede65041c148ebe21dd6075c17a1b
ZhangTaiYin <zhangtaiyin@imudges.com>
2018-08-18 12:00:46 +0800

    test

--- back-end/src/zhang/Login.java
+++ back-end/src/zhang/Login.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONCreator;
 
 /**
  * Servlet implementation class Login
@@ -27,6 +30,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("????");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -35,7 +39,9 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		doGet(request, response);
+		
 	}
 
 }
